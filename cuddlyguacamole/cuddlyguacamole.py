19bf9e18545e5b9328d432ed50ca3cdb56f06682
evabertalan <eva0bertalan@gmail.com>
2018-02-22 18:11:25 +0100

    create plot from given data

--- cuddlyguacamole/cuddlyguacamole.py
+++ cuddlyguacamole/cuddlyguacamole.py
@@ -1,41 +1,50 @@
 import load
 import system
 import numpy as np
+import plot
+import time
 
 temp = 120 #these could be somwhere more general place
 dim = 3
 
-r_cut_LJ = 2.5
+r_cut_LJ = 6
 r_skin_LJ = 0
-
-bsize=5 #??????
 
 n_opt_max = 30
 n_steps_opt = 1000
 tol_opt = 1/100
 n_reuse_nblist = 1
 n_skip = int(n_steps_opt/50)
-width = bsize / 1000
 save_system_history = True
 
 n_steps_sim = 5*n_steps_opt
 
 def optimize(filename='sodium-chloride-example.npz'):
+    start = time.time()
     print('optimize')
     parameters, positions, types, boxsize = load.load_input_file(filename)
+    width = boxsize[1]/1000
     particles = create_particle_object(parameters, positions, types)
-    boxsize = 5*np.ones(3) #remove it when size bug is solved
     box = system.Box(dim, boxsize, particles, temp)
+    # return system.Box(dim, boxsize, particles, temp)
 
     box.compute_LJneighbourlist(r_cut_LJ, r_skin_LJ)
     box.compute_LJ_potential(r_cut_LJ, r_skin_LJ)
     box.optimize(n_opt_max, n_steps_opt, tol_opt, n_reuse_nblist, n_skip, width, save_system_history, r_cut_LJ, r_skin_LJ)
+    box.simulate(n_steps_sim, n_reuse_nblist, n_skip, width, save_system_history, r_cut_LJ, r_skin_LJ)
+
+    pos_history = box.pos_history
+    pot_history = np.asarray(box.pot_history)
+
+    plot.create_plot(boxsize[1], pos_history, pot_history, r_cut_LJ, n_steps_opt, n_skip)
+    print('running time:',time.time() - start)
+
 
 def simulate(filename='sodium-chloride-example.npz'):
     print('simulate')
     parameters, positions, types, boxsize = load.load_input_file(filename)
+    width = boxsize[1]/1000
     particles = create_particle_object(parameters, positions, types)
-    boxsize = 5*np.ones(3) #remove it when size bug is solved
     box = system.Box(dim, boxsize, particles, temp)
 
     box.compute_LJneighbourlist(r_cut_LJ, r_skin_LJ)
@@ -55,4 +64,4 @@ def create_particle_object(parameters, positions, types):
 
     return particles
 
-# optimize('sodium-chloride-example.npz')
+optimize('sodium-chloride-example.npz')
