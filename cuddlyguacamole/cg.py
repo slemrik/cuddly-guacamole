7970ae6e6eeeefeca77e5674ea41a9ea2b634eb9
slemrik <henrik.gjoertz@fu-berlin.de>
2018-02-23 11:21:08 +0100

    Update cg.py

--- cuddlyguacamole/cg.py
+++ cuddlyguacamole/cg.py
@@ -2,9 +2,9 @@ import cuddlyguacamole
 import test.generate_test_system
 import numpy as np
 
-#r_cut_LJ = 2.5
+#r_cut_LJ = 5
 #r_skin_LJ = 0
-#testbox = test.generate_test_system.generate_test_system(3, 15*np.ones(3), 64, 200, 0, 1, 1)
+#testbox = test.generate_test_system.generate_test_system(3, 15*np.ones(3), 128, 200, 0, 2, 1)
 #testbox.compute_LJneighbourlist(r_cut_LJ, r_skin_LJ)
 #testbox.compute_LJ_potential(r_cut_LJ, r_skin_LJ)
 #testbox = cuddlyguacamole.optimize(box = testbox, to_plot=True)
